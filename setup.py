93e4bc5d17d1a920486b851e1fb7a588c906acce
Guy Kaplan <gkpln3@gmail.com>
2024-12-03 22:42:59 +0200

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='safe-pip',
-    version='1.0.0',
+    version='1.0.1',
     author='Guy Kaplan',
     author_email='gkpln3@gmail.com',
     description='A safer pip that checks package health before installation.',
